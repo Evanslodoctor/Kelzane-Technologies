1c7af72ec53f21c895adb6137a27f3b732c6b345
Evans Chaun <75381404+Evanslodoctor@users.noreply.github.com>
2024-12-05 20:29:10 +0300

    Updates

--- admin/assets/js/demo.js
+++ admin/assets/js/demo.js
@@ -192,8 +192,8 @@ Circles.create({
 //Notify
 $.notify({
 	icon: 'icon-bell',
-	title: 'Kaiadmin',
-	message: 'Premium Bootstrap 5 Admin Dashboard',
+	title: 'Welcome',
+	message: 'Kelzane Technologies Admin Pannel',
 },{
 	type: 'secondary',
 	placement: {
